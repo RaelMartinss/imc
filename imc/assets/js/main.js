c409038058fb5c37b93c1f09401973ae34dde371
unknown <raelpgm2@gmail.com>
2021-08-11 19:01:29 -0300

    primeiro

---
+++ imc/assets/js/main.js
@@ -0,0 +1,68 @@
+//Capturar evento de submit do formulario
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    //pegando o valor de altura e peso 
+
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    //verifica se os valores passados estão conforme foi pedido no front-end
+    if (!peso) {
+        setResultado('Peso Invalido', false);
+        return;
+    }
+    if (!altura) {
+        setResultado('Altura Invalida', false);
+        return;
+    }
+    //Captura os valores recebidos pelas funções 
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    // devolver o resultado para o front-end
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+    setResultado(msg, true);
+
+});
+
+function getNivelImc(imc) {
+    const nivel = ['const peso', 'Peso normal', 'Sobrepeso', 'Obesidade Grau 1', , 'Obesidade Grau 2', 'Obesidade Grau 3'];
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
+function getImc(peso, altura){
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
+function criarP() {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado(msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+
+    const p = criarP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}
